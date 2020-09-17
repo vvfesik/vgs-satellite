ead99e4946c15f2227eb44af026db47c43e3d4c6
Andrew <a.slepakurov@gmail.com>
2020-09-17 20:00:30 +0300

    ALias pregen (#39)

--- satellite/service/alias_manager.py
+++ satellite/service/alias_manager.py
@@ -1,3 +1,5 @@
+import uuid
+
 from satellite.vault.generator import generator_map
 
 from satellite.model.base import Session
@@ -19,11 +21,13 @@ class AliasManager:
         if alias_entity:
             return alias_entity.public_alias
         alias_generator_type = generator_map.get(alias_generator)
+        alias_id = str(uuid.uuid4())
         if not alias_generator_type:
             raise RedactFailed(f'{alias_generator} can\'t be used as a alias generator. '
                          f'Possible values: {str(alias_generator.keys())}')
-        public_alias = alias_generator_type.generate(id)
-        alias = Alias(value=value,
+        public_alias = alias_generator_type.generate(alias_id)
+        alias = Alias(id=alias_id,
+                      value=value,
                       alias_generator=alias_generator,
                       public_alias=public_alias)
         self.session.add(alias)
